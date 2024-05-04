8553b5f879712a17157f38266d9a96afabd85cd4
Yuvraj Singh Rathore <rathore.yuvraj19@gmail.com>
2024-05-04 09:35:42 +0530

    data base added

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "../_components/PageHeader";
+
+export default function AdminProductsPage() {
+  return (
+    <>
+      <div className="flex justify-between items-center gap-4">
+        <PageHeader>Products</PageHeader>
+      </div>
+    </>
+  );
+}
